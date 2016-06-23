6edb477cbe16672d03a76f4d20a2a8509a97096c
Padraig Gleeson <p.gleeson@gmail.com>
2016-06-23 12:28:40 +0100

    Test scaling

--- examples/test_scaling.py
+++ examples/test_scaling.py
@@ -2,15 +2,15 @@ from Balanced import generate
 
 
 nml_doc, nml_file_name, lems_file_name = generate(num_bbp =10,
-     scalePops = 2,
+     scalePops = 20,
      scalex=2,
      scalez=2,
      connections=True,
      format='hdf5')
      
-from neuroml.loaders import NeuroMLHDF5Loader
+from neuroml.loaders import NeuroMLHdf5Loader
 
-nml_doc2 = NeuroMLHDF5Loader.load(nml_file_name)
+nml_doc2 = NeuroMLHdf5Loader.load(nml_file_name)
 
 
 for doc in [nml_doc,nml_doc2]:
