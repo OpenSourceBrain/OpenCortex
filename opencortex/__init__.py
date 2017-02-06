62e681386df819e60eac6ee46ddf51329f0231c6
Padraig Gleeson <p.gleeson@gmail.com>
2017-02-06 17:31:39 +0000

    Update to v0.0.8

--- opencortex/__init__.py
+++ opencortex/__init__.py
@@ -3,7 +3,7 @@ print("          Please note that OpenCortex is in a very preliminary state ");
 print("          and the API is subject to change without notice!  ")
 print("*********************************************************************************************\n")
 
-__version__ = '0.0.7'
+__version__ = '0.0.8'
 
 
 verbose = False
