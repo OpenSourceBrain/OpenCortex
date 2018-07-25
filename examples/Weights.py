82493a20aba53f29e54054f57654f91ef496b56e
Padraig Gleeson <p.gleeson@gmail.com>
2018-07-25 10:46:02 +0100

    Update weights

--- examples/Weights.py
+++ examples/Weights.py
@@ -1,6 +1,11 @@
 import opencortex.core as oc
 import sys
 
+from neuroml import GradedSynapse
+from neuroml import SilentSynapse
+from neuroml import ContinuousProjection
+from neuroml import ContinuousConnectionInstanceW
+
 info = '''
 Simple network having multiple connections (chemical/gap junction/analog) with variable weights
 '''
@@ -26,26 +31,47 @@ def generate(reference = "Weights",
                                                   0,0,0, xDim,yDim,zDim,
                                                   color='.8 0 0')
 
-    pop_post_chem = oc.add_population_in_rectangular_region(network, 'pop_post_chem',
+    pop_post_chem_exc = oc.add_population_in_rectangular_region(network, 'pop_post_chem_exc',
                                                   cell_id, num_each,
                                                   0,yDim,0, xDim,yDim,zDim,
                                                   color='0 0 .8')
-                                                  
-    pop_post_gap = oc.add_population_in_rectangular_region(network, 'pop_post_gap',
+
+    pop_post_chem_inh = oc.add_population_in_rectangular_region(network, 'pop_post_chem_inh',
                                                   cell_id, num_each,
                                                   xDim,yDim,0, xDim,yDim,zDim,
+                                                  color='0 .8 .8')
+                                                  
+    pop_post_cont = oc.add_population_in_rectangular_region(network, 'pop_post_cont',
+                                                  cell_id, num_each,
+                                                  xDim,0,0, xDim,yDim,zDim,
                                                   color='0 .8 0')
 
     ampa_syn = oc.add_exp_two_syn(nml_doc, id="AMPA_syn", 
                              gbase="10nS", erev="0mV",
-                             tau_rise="0.003s", tau_decay="0.0031s")
+                             tau_rise="2ms", tau_decay="10ms")
+                             
+    gaba_syn = oc.add_exp_two_syn(nml_doc, id="GABA_syn", 
+                             gbase="10nS", erev="-80mV",
+                             tau_rise="3ms", tau_decay="30ms")
 
     gj_syn = oc.add_gap_junction_synapse(nml_doc, id="gj0", 
-                             conductance="1nS")
+                             conductance=".05nS")
+                             
+    
+    analog_syn = GradedSynapse(id='analog_syn',
+                             conductance="10nS",
+                             delta="5mV",
+                             Vth="-35mV",
+                             k="0.025per_ms",
+                             erev="0mV")
+    silent_syn = SilentSynapse(id="silent1")
+    
+    nml_doc.graded_synapses.append(analog_syn)
+    nml_doc.silent_synapses.append(silent_syn)
 
 
     pfs = oc.add_poisson_firing_synapse(nml_doc, id="poissonFiringSyn",
-                                       average_rate="20 Hz", synapse_id=ampa_syn.id)
+                                       average_rate="10 Hz", synapse_id=ampa_syn.id)
 
     oc.add_inputs_to_population(network, "Stim0",
                                 pop_pre, pfs.id, all_cells=True)
@@ -53,34 +79,72 @@ def generate(reference = "Weights",
 
     if connections:
         
-        proj_chem = oc.add_probabilistic_projection(network,
-                                "proj_chem",
+        proj_chem_exc = oc.add_probabilistic_projection(network,
+                                "proj_chem_exc",
                                 pop_pre,
-                                pop_post_chem,
+                                pop_post_chem_exc,
                                 ampa_syn.id,
                                 0.3,
                                 weight=1,
                                 delay=5)
                                 
-        for conn in proj_chem.connection_wds:
+        for conn in proj_chem_exc.connection_wds:
             if conn.get_pre_cell_id() < 3 and conn.get_post_cell_id() < 3:
                 conn.weight = 0.5
             print conn
-            
-        proj_gap = oc.add_targeted_electrical_projection(nml_doc, 
-                                        network,
-                                        "proj_gap",
-                                        pop_pre,
-                                        pop_post_gap,
-                                        targeting_mode='convergent',
-                                        synapse_list=[gj_syn.id],
-                                        pre_segment_group = 'soma_group',
-                                        post_segment_group = 'soma_group',
-                                        number_conns_per_cell=3)
-                          
-        for conn in network.electrical_projections[0].electrical_connection_instance_ws:
-            conn.weight = conn.get_pre_cell_id() + conn.get_post_cell_id()
+        
+        proj_chem_inh = oc.add_probabilistic_projection(network,
+                                "proj_chem_inh",
+                                pop_pre,
+                                pop_post_chem_inh,
+                                gaba_syn.id,
+                                0.3,
+                                weight=1,
+                                delay=5)
+                                
+        for conn in proj_chem_inh.connection_wds:
+            if conn.get_pre_cell_id() < 3 and conn.get_post_cell_id() < 3:
+                conn.weight = 2
             print conn
+            
+        
+        proj_cont = ContinuousProjection(id='proj_cont', \
+                           presynaptic_population=pop_pre.id,
+                           postsynaptic_population=pop_post_cont.id)
+        network.continuous_projections.append(proj_cont)
+        
+        for i in range(pop_pre.get_size()):
+            for j in range(pop_post_cont.get_size()):
+                
+                conn0 = ContinuousConnectionInstanceW(id='%s'%(j+i*pop_pre.get_size()), \
+                           pre_cell='../%s/%s/%s'%(pop_pre.id,i,cell_id),
+                           post_cell='../%s/%s/%s'%(pop_post_cont.id,j,cell_id),
+                           pre_component=silent_syn.id,
+                           post_component=analog_syn.id,
+                           weight=(i+j)/10)
+                proj_cont.continuous_connection_instance_ws.append(conn0)
+        
+        
+            
+        gj_pops = [pop_post_chem_exc, pop_post_chem_inh, pop_post_cont]
+        
+        for pre in gj_pops:
+            for post in gj_pops:
+                
+                proj_gap = oc.add_targeted_electrical_projection(nml_doc, 
+                                                network,
+                                                "proj_",
+                                                pre,
+                                                post,
+                                                targeting_mode='convergent',
+                                                synapse_list=[gj_syn.id],
+                                                pre_segment_group = 'soma_group',
+                                                post_segment_group = 'soma_group',
+                                                number_conns_per_cell=3)
+
+                for conn in network.electrical_projections[-1].electrical_connection_instance_ws:
+                    conn.weight = conn.get_pre_cell_id() + conn.get_post_cell_id()
+                    print conn
 
 
 
