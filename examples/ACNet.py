34b704679d5855c219e772d010e706e7a8a96c21
Padraig Gleeson <p.gleeson@gmail.com>
2021-02-23 09:40:46 +0000

    ACNet tests updated & retested for generation on py3

--- examples/ACNet.py
+++ examples/ACNet.py
@@ -24,7 +24,7 @@ def generate(reference = "ACNet",
 
     oc.include_opencortex_cell(nml_doc, 'acnet2/pyr_4_sym.cell.nml')
     oc.include_opencortex_cell(nml_doc, 'acnet2/bask.cell.nml')
-    
+
     xDim = 500*scalex
     yDim = 50*scaley
     zDim = 500*scalez
@@ -38,27 +38,27 @@ def generate(reference = "ACNet",
                                                   'bask', num_bask,
                                                   0,yDim,0, xDim,yDim+yDim,zDim,
                                                   color='0 0 .8')
-                   
-    ampa_syn = oc.add_exp_two_syn(nml_doc, id="AMPA_syn", 
+
+    ampa_syn = oc.add_exp_two_syn(nml_doc, id="AMPA_syn",
                              gbase="30e-9S", erev="0mV",
                              tau_rise="0.003s", tau_decay="0.0031s")
 
-    ampa_syn_inh = oc.add_exp_two_syn(nml_doc, id="AMPA_syn_inh", 
+    ampa_syn_inh = oc.add_exp_two_syn(nml_doc, id="AMPA_syn_inh",
                              gbase="0.15e-9S", erev="0mV",
                              tau_rise="0.003s", tau_decay="0.0031s")
 
-    gaba_syn = oc.add_exp_two_syn(nml_doc, id="GABA_syn", 
+    gaba_syn = oc.add_exp_two_syn(nml_doc, id="GABA_syn",
                              gbase="0.6e-9S", erev="-0.080V",
                              tau_rise="0.005s", tau_decay="0.012s")
 
-    gaba_syn_inh = oc.add_exp_two_syn(nml_doc, id="GABA_syn_inh", 
+    gaba_syn_inh = oc.add_exp_two_syn(nml_doc, id="GABA_syn_inh",
                              gbase="0S", erev="-0.080V",
                              tau_rise="0.003s", tau_decay="0.008s")
 
     pfs = oc.add_poisson_firing_synapse(nml_doc, id="poissonFiringSyn",
                                        average_rate="30 Hz", synapse_id=ampa_syn.id)
-                                
-                                
+
+
     total_conns = 0
     if connections:
 
@@ -73,7 +73,7 @@ def generate(reference = "ACNet",
                                         post_segment_group = 'dendrite_group',
                                         number_conns_per_cell=7,
                                         delays_dict = {this_syn:global_delay})
-        if proj:                           
+        if proj:
             total_conns += len(proj[0].connection_wds)
 
         this_syn=ampa_syn_inh.id
@@ -87,7 +87,7 @@ def generate(reference = "ACNet",
                                         post_segment_group = 'all',
                                         number_conns_per_cell=21,
                                         delays_dict = {this_syn:global_delay})
-        if proj:                           
+        if proj:
             total_conns += len(proj[0].connection_wds)
 
         this_syn=gaba_syn.id
@@ -101,7 +101,7 @@ def generate(reference = "ACNet",
                                         post_segment_group = 'all',
                                         number_conns_per_cell=21,
                                         delays_dict = {this_syn:global_delay})
-        if proj:                           
+        if proj:
             total_conns += len(proj[0].connection_wds)
 
         this_syn=gaba_syn_inh.id
@@ -115,27 +115,27 @@ def generate(reference = "ACNet",
                                         post_segment_group = 'all',
                                         number_conns_per_cell=5,
                                         delays_dict = {this_syn:global_delay})
-        if proj:                           
+        if proj:
             total_conns += len(proj[0].connection_wds)
-            
-            
+
+
     oc.add_targeted_inputs_to_population(network, "Stim0",
-                                pop_pyr, pfs.id, 
+                                pop_pyr, pfs.id,
                                 segment_group='soma_group',
                                 number_per_cell = 1,
                                 all_cells=True)
-        
-        
+
+
     if num_pyr != 48 or num_bask!=12:
         new_reference = '%s_%scells_%sconns'%(nml_doc.id,num_pyr+num_bask,total_conns)
         network.id = new_reference
         nml_doc.id = new_reference
-        
+
     nml_file_name = '%s.net.%s'%(network.id,'nml.h5' if format == 'hdf5' else 'nml')
     target_dir = 'HDF5/' if format == 'hdf5' else './'
-    
-    oc.save_network(nml_doc, 
-                    nml_file_name, 
+
+    oc.save_network(nml_doc,
+                    nml_file_name,
                     validate=(format=='xml'),
                     format = format,
                     target_dir=target_dir)
@@ -167,29 +167,30 @@ def generate(reference = "ACNet",
                     gen_plots_for_quantities[display].append(quantity)
                     gen_saves_for_quantities[file_].append(quantity)
 
-    lems_file_name = oc.generate_lems_simulation(nml_doc, 
-                            network, 
-                            target_dir+nml_file_name, 
-                            duration =      duration, 
+    lems_file_name = oc.generate_lems_simulation(nml_doc,
+                            network,
+                            target_dir+nml_file_name,
+                            duration =      duration,
                             dt =            0.025,
                             gen_plots_for_all_v = False,
                             gen_plots_for_quantities = gen_plots_for_quantities,
                             gen_saves_for_all_v = False,
                             gen_saves_for_quantities = gen_saves_for_quantities,
-                            target_dir=target_dir)
-                                
+                            target_dir=target_dir,
+                            report_file_name='report.acnet.txt')
+
     return nml_doc, nml_file_name, lems_file_name
 
 
 if __name__ == '__main__':
-    
+
     if '-test' in sys.argv:
-        
+
         generate(num_pyr = 2,
                  num_bask=2,
                  duration = 500,
                  global_delay = 2)
-                 
+
         generate(num_pyr = 1,
                  num_bask=0,
                  duration = 500,
