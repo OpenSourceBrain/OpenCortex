3ab9a6532423ef0dec21c5ab2ecbad0635746247
Padraig Gleeson <p.gleeson@gmail.com>
2025-08-20 12:29:41 +0100

    Test more

--- setup.py
+++ setup.py
@@ -1,46 +1,53 @@
 from setuptools import setup
 
 import opencortex
+
 version = opencortex.__version__
 
 setup(
-    name='OpenCortex',
+    name="OpenCortex",
     version=version,
-    author='Rokas Stanislovas and Padraig Gleeson',
-    author_email='p.gleeson@gmail.com',
-    packages = ['opencortex',
-                'opencortex.core',
-                'opencortex.build',
-                'opencortex.test',
-                'opencortex.utils'],
-    package_data={
-        'opencortex': [
-            '../NeuroML2/prototypes/iaf/*',
-            '../NeuroML2/prototypes/izhikevich/*',
-            '../NeuroML2/prototypes/Thalamocortical/*',
-            '../NeuroML2/prototypes/BlueBrainProject_NMC/*',
-            '../NeuroML2/prototypes/AllenInstituteCellTypesDB_HH/*',
-            '../NeuroML2/prototypes/L23Pyr_SmithEtAl2013/*',
-            '../NeuroML2/prototypes/acnet2/*']},
-
-    url='https://github.com/OpenSourceBrain/OpenCortex',
-    license='LICENSE.lesser',
-    description='A framework for building cortical network models',
-    long_description=open('README.md').read(),
-    install_requires=[
-        'pyNeuroML>=0.3.18', # sets dependencies for other neuroml libs
-        'matplotlib',
-        'tables'],
-    dependency_links=[
-      'git+https://github.com/NeuralEnsemble/libNeuroML.git@development#egg=libNeuroML-0.2.10'
+    author="Rokas Stanislovas and Padraig Gleeson",
+    author_email="p.gleeson@gmail.com",
+    packages=[
+        "opencortex",
+        "opencortex.core",
+        "opencortex.build",
+        "opencortex.test",
+        "opencortex.utils",
     ],
-    classifiers = [
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
-        'Topic :: Scientific/Engineering']
+    package_data={
+        "opencortex": [
+            "../NeuroML2/prototypes/iaf/*",
+            "../NeuroML2/prototypes/izhikevich/*",
+            "../NeuroML2/prototypes/Thalamocortical/*",
+            "../NeuroML2/prototypes/BlueBrainProject_NMC/*",
+            "../NeuroML2/prototypes/AllenInstituteCellTypesDB_HH/*",
+            "../NeuroML2/prototypes/L23Pyr_SmithEtAl2013/*",
+            "../NeuroML2/prototypes/acnet2/*",
+        ]
+    },
+    url="https://github.com/OpenSourceBrain/OpenCortex",
+    license="LICENSE.lesser",
+    description="A framework for building cortical network models",
+    long_description=open("README.md").read(),
+    install_requires=[
+        "pyNeuroML>=0.3.18",  # sets dependencies for other neuroml libs
+        "matplotlib",
+        "tables",
+    ],
+    dependency_links=[
+        "git+https://github.com/NeuralEnsemble/libNeuroML.git@development#egg=libNeuroML-0.2.10"
+    ],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Scientific/Engineering",
+    ],
 )
